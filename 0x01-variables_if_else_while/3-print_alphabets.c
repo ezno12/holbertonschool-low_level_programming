3f58f7a21fa54d7a4aceb113b3b0bbe6a8536225
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 14:01:03 +0100

    pr alph

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print alphabet
+*
+* Return:0  (sucess)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; ++c)
+{
+putchar(c);
+}
+
+{
+putchar('\n');
+}
+return (0);
+}
