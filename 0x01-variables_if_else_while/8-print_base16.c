b2ac218cc224f2eeedfa2bcffe702e66d609ebf6
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 21:18:44 +0100

    base16 added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print alphabet
+*
+* Return:0  (sucess)
+*/
+int main(void)
+{
+int c;
+for (c = 48; c < 57; ++c)
+{
+putchar(c);
+}
+for (c = 65; c < 71; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+
+return (0);
+}
