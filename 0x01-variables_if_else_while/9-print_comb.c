7ee4a9d9f788eb249479c71de88b7167aada595f
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 21:55:33 +0100

    print comb added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print alphabet
+*
+* Return:0  (sucess)
+*/
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; ++c)
+{
+if (c != '0')
+{
+putchar(',');
+putchar(' ');
+}
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
