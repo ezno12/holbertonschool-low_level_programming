51fda36f19fc6485d6d78efc0146d8eb962b4cb9
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 20:21:38 +0100

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
 * main - print alphabet
@@ -14,6 +12,7 @@ for (c = 48; c <= 57; ++c)
 putchar(c);
 }
 putchar('\n');
-}
+
 return (0);
 }
+
