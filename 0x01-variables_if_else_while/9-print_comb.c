857ac80a7b99207113fab80625b16f91a44ce1d1
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 22:13:21 +0100

    comb1 corrected

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,20 +9,17 @@
 int main(void)
 {
 int c;
-int d;
+
 for (c = '0'; c <= '9'; ++c)
 {
-for (d = '0'; d <= '9'; ++d)
-{
-if (d != '0' || c != '0')
+if (c != '0')
 {
 putchar(',');
 putchar(' ');
 }
 putchar(c);
-putchar(d);
 }
-}
+
 putchar('\n');
 return (0);
 }
