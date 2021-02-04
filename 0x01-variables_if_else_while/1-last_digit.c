236428672d0946b0ad1978818f681c50a89fb563
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 12:29:20 +0100

    lst digit upd

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
