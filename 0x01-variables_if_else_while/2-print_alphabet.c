0eec3512eada4ab1ab53da4b53d932a4328015d8
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 13:16:33 +0100

    updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
 *return:0 :is sucess
 */
 int main(void){
-    char c;
-    for (c = 'a'; c <= 'z'; ++c)
-        putchar(c);
-    return 0;
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+return 0;
 }
