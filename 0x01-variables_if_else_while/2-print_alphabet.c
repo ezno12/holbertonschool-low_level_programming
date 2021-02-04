dfcc803dd7d809e63c1ff24ebeff6cae26aa2383
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 13:07:44 +0100

    print alph uploded

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+*main - main function
+*
+*return:0 :is sucess
+*/
+int main() {
+    char c;
+    for (c = 'a'; c <= 'z'; ++c)
+        putchar(c);
+    return 0;
+}
