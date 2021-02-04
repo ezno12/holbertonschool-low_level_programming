8218b1486715039aef9c35949bd4daa004de52d9
tennin12 <bassembenyhia12@gmail.com>
2021-02-04 15:00:03 +0100

    update new files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print alphabet
+*
+* Return:0  (sucess)
+*/
+int main(void)
+{
+unsigned int c;
+for (c = 0; c <= 9; ++c)
+{
+    printf("%i", c);
+}
+putchar('\n');
+return (0);
+}
