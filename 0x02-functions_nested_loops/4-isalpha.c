19515b2159726a22911a6a3c687d7a2a9cc7f47e
tennin12 <bassembenyhia12@gmail.com>
2021-02-09 11:44:44 +0100

    new update for 4file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,7 @@
 */
 int _islower(int c)
 {
-if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 _putchar('1');
 else
 _putchar('0');
