457139c76fb9406c611ec9b140a1dfe4ee96cff2
tennin12 <bassembenyhia12@gmail.com>
2021-02-09 20:13:14 +0100

    update2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
-* main -  function bugger, that prints if integer is positive or negative.
-*
+* main -Buggers function, that prints if integer is positive or negative
 * Return: 0
 */
 
 int main(void)
 {
+	int i;
 
-int i;
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
