04e95de439bcdbcfb61937d032e0d39aad79ad10
tennin12 <bassembenyhia12@gmail.com>
2021-02-09 09:26:13 +0100

    0 file added

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,12 +6,15 @@
 */
 int main(void)
 {
-int i, arra[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-i = 0;
-for (i = 0; i <= 9; ++i)
-{
-_putchar(arra[i]);
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
 return (0);
 }
