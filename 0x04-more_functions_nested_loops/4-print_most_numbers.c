d95343fddc17da5e0f5636fab355809c1f17f16d
tennin12 <bassembenyhia12@gmail.com>
2021-02-12 18:41:55 +0100

    update holberton f

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,16 @@
-#include "holberton"
+
+#include "holberton.h"
 /**
-* print_most_numbers - print number sans2 & 4
-*
-* Retrun: void.
-*/
+ * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
+ *
+ * Return:void
+ */
 void print_most_numbers(void)
 {
-char c;
-for (c = 48; c <= 57; ++c)
+int c;
+for (c = '0'; c <= '9'; c++)
 {
-if ( c != 50 && c != 52)
+if ((c != '2') && (c != '4'))
 _putchar(c);
 }
 _putchar('\n');
