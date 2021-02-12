d1c87a5639b8c5269a475a2a2c5dbcf400be0fa8
tennin12 <bassembenyhia12@gmail.com>
2021-02-12 18:51:06 +0100

    f5 fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,22 @@
 #include "holberton"
-/***
+/**
 * more_numbers : print number sans2 & 4
 *
 * Retrun: void.
 */
 void more_numbers(void)
 {
-int c;
+int c, n;
+for (n = 0; n <= 10; n++)
+{
 for (c = 0; c <= 14; ++c)
 {
 if ( c > 9)
 {
-_putchar(c /10 + '0');
+_putchar(c / 10 + '0');
 }
-_putchar(c);
+_putchar(c % 10 + '0');
 }
 _putchar('\n');
+}
 }
