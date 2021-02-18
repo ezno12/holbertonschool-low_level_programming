357e036f6e32b92eb9216ec1f04ee3f3726e9af8
tennin12 <bassembenyhia12@gmail.com>
2021-02-18 12:27:03 +0100

    f5 corrected

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,16 @@
 */
 void rev_string(char *s)
 {
-    int g, numb;
-    int tmpry = 0;
-
-    for(numb=0; s[numb] != 0; numb++);
-    for(g = 0; g <numb/2; g++)
-    {
-        tmpry = s[g];
-        s[g]=s[numb - 1 - g];
-        s[numb - 1 - g] = tmpry;
-    }
-    for(g = 0; g < numb; g++)
-        _putchar(*s[g]);
-    _putchar('\n');
+int g, numb;
+int tmpry = 0;
+for (numb = 0; s[numb] != 0; numb++);
+for (g = 0; g < numb / 2; g++)
+{
+tmpry = s[g];
+s[g] = s[numb - 1 - g];
+s[numb - 1 - g] = tmpry;
+}
+for (g = 0; g < numb; g++)
+_putchar(s[g]);
+_putchar('\n');
 }
