080469d5ad9474748235946cac89ada573bcfa84
tennin12 <bassembenyhia12@gmail.com>
2021-02-18 11:37:53 +0100

    f8 added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_array - print array value in order.
+ *@a : pointer.
+ *@n : integer
+ * Return: void (sucess).
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i <= n ; ++i)
+{
+printf("%d, ", *a);
+}
+}
