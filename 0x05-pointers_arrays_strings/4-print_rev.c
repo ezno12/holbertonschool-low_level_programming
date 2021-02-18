e4e85a7d9560b7b4715cb300215970c36d65aa31
tennin12 <bassembenyhia12@gmail.com>
2021-02-18 21:01:05 +0100

    f4 fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
+
 #include "holberton.h"
 /**
-* print_rev - print a reversed string
-* @s:pointer char
-* return:void
+* print_rev - prints string in reverse.
+* @s: pionter.
+* Return: void.
 */
 void print_rev(char *s)
 {
-int i, j, k;
-char rev[];
-for(i = 0; s[i] != '\0'; i++);
+char *c;
+c = s;
+while (*s != '\0')
 {
-k = i-1;
+s++;
 }
-for(j = 0; j <= i-1; j++)
+while (s != c)
 {
-rev[j] = s[k];
-k--;
-_putchar(rev[j]);
+s--;
+_putchar(*s);
 }
 _putchar('\n');
 }
