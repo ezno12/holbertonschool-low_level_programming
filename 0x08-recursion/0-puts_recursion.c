c884089af543d5cafe330fe18b483b360ff52cd3
tennin12 <bassembenyhia12@gmail.com>
2021-02-24 13:10:20 +0100

    updte f0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,8 +11,16 @@ void _puts_recursion(char *s)
 int i = 0;
 while (s[i] != '\0')
 {
+if (s[i] == '\0')
+{
+return;
+}
+else
+{
 _putchar(*s);
-++s;
+_puts_recursion(s);
+}
+++i;
 }
 _putchar('\n');
 }
