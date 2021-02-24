2fa7b92cf5faced2a2a1348e18f83bc9b58520f9
tennin12 <bassembenyhia12@gmail.com>
2021-02-24 23:03:47 +0100

    update and add the new files

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* st - returns the prime number.
+* @i: int
+* @j: int
+* Return: int
+*/
+int st(int i, int j)
+{
+if (i % j == 0)
+return (0);
+if (j > (i / 2))
+return (1);
+return (st(i, ++j));
+}
+/**
+* is_prime_number - returns prime number.
+* @n: integer.
+* Return: integer.
+*/
+int is_prime_number(int n)
+{
+return (st(2, n));
+}
