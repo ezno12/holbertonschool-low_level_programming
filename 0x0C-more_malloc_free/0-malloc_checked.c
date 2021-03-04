27be835c27292223a9316c780d83a49c8ddd2f87
tennin12 <bassembenyhia12@gmail.com>
2021-03-04 20:51:18 +0100

    f0 added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*malloc_checked - function allocate memory
+*
+*@b: int
+*
+* Return:viod.
+*/
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
