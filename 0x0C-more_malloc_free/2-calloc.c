a87d22ac28370ba2d19b44bb6a7c635e3e7d38f3
tennin12 <bassembenyhia12@gmail.com>
2021-03-04 22:41:04 +0100

    f2 correctd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-char *s
+char *s;
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
@@ -20,7 +20,7 @@ return (NULL);
 s = malloc(nmemb * size);
 if (s == NULL)
 return (NULL);
-for (i = 0; i < nemb * size; i++)
+for (i = 0; i < nmemb * size; i++)
 {
 s[i] = '\0';
 }
