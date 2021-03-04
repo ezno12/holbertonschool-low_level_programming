9368b608b9bf9bc1727116034a9e8badcc442781
tennin12 <bassembenyhia12@gmail.com>
2021-03-04 22:38:06 +0100

    f3 added&fixed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,16 +11,16 @@ int *array_range(int min, int max)
 {
 int i;
 int j = 0;
-int *ptr
+int *ptr;
 if (min > max)
 return (NULL);
 ptr = malloc(max - min + 1);
-if (prt == NULL)
+if (ptr == NULL)
 return (NULL);
 for (i = min; i < max + 1; i++)
 {
-p[j] = i;
+ptr[j] = i;
 j++;
 }
-return (p);
+return (ptr);
 }
