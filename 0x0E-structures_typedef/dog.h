2ad18195dd2390068574f18aa82ff54b090949e1
tennin12 <bassembenyhia12@gmail.com>
2021-03-08 21:52:35 +0100

    f1 added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,12 @@
 *
 * Description: struct dog contains type dogs
 */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner
-};
-typedef struct dog dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
