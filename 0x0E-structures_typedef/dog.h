39cc1cdf94b73a121d0a75654b113b18a71325c0
tennin12 <bassembenyhia12@gmail.com>
2021-03-08 21:48:24 +0100

    f1 added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - dog type
+* @name: name of dog (pionter)
+* @age: age of the dog
+* @owner: dog owner (pionter)
+*
+* Description: struct dog contains type dogs
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
