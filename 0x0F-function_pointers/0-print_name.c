2907b61b9e98d726241700a7a9ea1055ea875df4
tennin12 <bassembenyhia12@gmail.com>
2021-03-10 21:04:10 +0100

    f0 added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - prints a name as is.
+*
+* @name: name of the person, char pointer.
+* @f: pionter to function.
+*
+* Return: viod (success).
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL || f != NULL)
+(*f)(name);
+}
