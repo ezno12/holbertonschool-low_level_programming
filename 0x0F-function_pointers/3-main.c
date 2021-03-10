079de0010f2ce15a8140754a68350649f5eb728d
tennin12 <bassembenyhia12@gmail.com>
2021-03-10 23:07:12 +0100

    allf files of task 3 added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+* main - performs simple operations
+* @argc: arg count
+* @argv: arg value
+*
+* Return:int
+*/
+int main(int argc, char *argv[])
+{
+int x, y, z;
+int (*res)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+x = atoi(argv[1]);
+y = atoi(argv[3]);
+fun = get_op_func(argv[2]);
+if (res == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+z = res(x, y);
+printf("%d\n", z);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* op_add - sum fun
+*
+* @a: int
+* @b: int
+*
+* Return: int (success)
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - sub fun
+*
+* @a: int
+* @b: int
+*
+* Return: int (success)
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* op_mul - mul fun
+*
+* @a: int
+* @b: int
+*
+* Return: int (success)
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div - div fun
+*
+* @a: int
+* @b: int
+*
+* Return: int (success)
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+* op_mod - mod fun
+*
+* @a: int
+* @b: int
+*
+* Return: int (success)
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
