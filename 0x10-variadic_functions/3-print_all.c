045a22f66a3e04e5b9369867a8afe4b407a2ac77
tennin12 <bassembenyhia12@gmail.com>
2021-03-11 23:04:39 +0100

    f3 added

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+* print_all - print anything
+*
+*@format: argement
+*
+* Return: viod (success).
+*/
+void print_all(const char * const format, ...)
+{
+unsigned int i = 0, length = 0;
+char *s, *p;
+va_list ap;
+while (format[length] != '\0')
+length++;
+va_start(ap, format);
+while (i < length)
+{
+p = ", ";
+if (i >= length - 1)
+p = "";
+switch (format[i])
+{
+case 'c':
+printf("%c%s", va_arg(ap, int),  p);
+break;
+case 'i':
+printf("%i%s", va_arg(ap, int),  p);
+break;
+case 'f':
+printf("%f%s", va_arg(ap, double),  p);
+break;
+case 's':
+s = va_arg(ap, char*);
+if (s == NULL)
+{
+s = "";
+p = "";
+}
+printf("%s%s", s,  p);
+break;
+default:
+p = "";
+break;
+}
+i++;
+}
+printf("\n");
+va_end(ap);
+}
