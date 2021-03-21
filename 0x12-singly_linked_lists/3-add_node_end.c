0bbad404b4aca7e1c670fb2dee396cf06c98efa9
tennin12 <bassembenyhia12@gmail.com>
2021-03-21 15:51:41 +0100

    f3 added and fixed

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,39 @@
 #include "lists.h"
 /**
- * _strlen - lenght of string
- * @s:char
- * Return:int
- */
-unsigned int _strlen(const char *s)
-{
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-continue;
-}
-return (i);
-}
-/**
- * add_node_end - add new elemnt in the end of list.
- *
- * @head: list pionter.
- * @str: pionter.
- * Return: new element.
- */
+* add_node_end - add new element in the end.
+*
+* @head: pionter to the first element in list.
+* @str: pionter to  char.
+*
+* Return: new in list.
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new;
-list_t *current;
-current = *head;
+list_t *new, *temp;
+int i;
 new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+;
 new->str = strdup(str);
-new->len = _strlen(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = i;
 new->next = NULL;
 if (*head == NULL)
 *head = new;
+else if ((*head)->next == NULL)
+(*head)->next = new;
 else
 {
-while (current->next != NULL)
-current = current->next;
-current->next = new;
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new;
 }
 return (new);
 }
