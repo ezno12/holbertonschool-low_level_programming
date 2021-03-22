8412346fd392e7d937ea113a1c7f6c769fb511ed
tennin12 <bassembenyhia12@gmail.com>
2021-03-22 18:30:33 +0100

    f6 added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* pop_listint - delet head of list.
+*
+* @head: pionter to list.
+*
+* Return: int (succes).
+*/
+int pop_listint(listint_t **head)
+{
+size_t save;
+listint_t *tmp;
+if (*head == NULL || head == NULL)
+return (0);
+save = (*head)->n;
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+return (save);
+}
