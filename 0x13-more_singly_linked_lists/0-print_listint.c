3eafc14a89d758c55bc422482bb38a220e07d8a0
tennin12 <bassembenyhia12@gmail.com>
2021-03-22 15:46:10 +0100

    f0 added

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* print_listint -print elements of list
+*
+* @h: head of list.
+*
+* Return: lenth of list.
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+while (h)
+{
+printf("%d\n", h->n);
+i++;
+h = h->next;
+}
+return (i);
+}
