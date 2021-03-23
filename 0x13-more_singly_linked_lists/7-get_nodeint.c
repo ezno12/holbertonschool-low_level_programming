6162193ab9a18721f945dff97345681972941091
tennin12 <bassembenyhia12@gmail.com>
2021-03-23 22:31:30 +0100

    f7 fixed & added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,17 +9,14 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int len = 0, i;
 listint_t *temp;
+unsigned int i, j;
 temp = head;
-while (head != NULL)
-{
+for (j = 0; head != NULL; j++)
 head = head->next;
-len++;
-}
-if (len < index)
+if (index > j)
 return (NULL);
-for (i = 1; i < len - index + 1; i++)
+for (i = 0; i < index; i++)
 {
 if (temp == NULL)
 return (NULL);
