e7e423777489f4c9e81a3f37a1e316cb952dda77
tennin12 <bassembenyhia12@gmail.com>
2021-03-25 14:12:03 +0100

    f0 fixed & added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int val = 0;
-int i = 0;
+int i;
 if (b == NULL)
 return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+}
+i = 0;
 while (b[i] == '0' || b[i] == '1')
 {
 val <<= 1;
