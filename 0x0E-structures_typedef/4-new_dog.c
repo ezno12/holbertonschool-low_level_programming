4447a9d1e79b1a4e50b849023de22d5cb3adf090
tennin12 <bassembenyhia12@gmail.com>
2021-04-09 17:22:41 +0100

    f4 coorected and added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
 * new_dog - creates a new dog.
 * @name: pointer to a char for name of dog
@@ -10,35 +9,33 @@
 **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nameLen, ownerLen, i;
-	dog_t *ndog;
-
-	ndog = (dog_t *)malloc(sizeof(dog_t));
-	if (ndog == NULL)
-		return (NULL);
-
-	nameLen = ownerLen = 0;
-	while (name[nameLen++])
-		;
-	while (owner[ownerLen++])
-		;
-	ndog->name = malloc(nameLen * sizeof(ndog->name));
-	if (ndog->name == NULL)
-	{
-		free(ndog);
-		return (NULL);
-	}
-	for (i = 0; i <= nameLen; i++)
-		ndog->name[i] = name[i];
-	ndog->age = age;
-	ndog->owner = malloc(ownerLen * sizeof(ndog->owner));
-	if (ndog->owner == NULL)
-	{
-		free(ndog->name);
-		free(ndog);
-		return (NULL);
-	}
-	for (i = 0; i <= ownerLen; i++)
-		ndog->owner[i] = owner[i];
-	return (ndog);
+int dgname, dgowner, i;
+dog_t *ndog;
+ndog = (dog_t *)malloc(sizeof(dog_t));
+if (ndog == NULL)
+return (NULL);
+dgname = dgowner = 0;
+while (name[dgname++])
+;
+while (owner[dgowner++])
+;
+ndog->name = malloc(dgname *sizeof(ndog->name));
+if (ndog->name == NULL)
+{
+free(ndog);
+return (NULL);
+}
+for (i = 0; i <= dgname; i++)
+ndog->name[i] = name[i];
+ndog->age = age;
+ndog->owner = malloc(dgowner * sizeof(ndog->owner));
+if (ndog->owner == NULL)
+{
+free(ndog->name);
+free(ndog);
+return (NULL);
+}
+for (i = 0; i <= dgowner; i++)
+ndog->owner[i] = owner[i];
+return (ndog);
 }
