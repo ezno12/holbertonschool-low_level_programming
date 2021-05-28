247d118ec610cbf257893d4c0ad240f510f44709
tennin12 <bassembenyhia12@gmail.com>
2021-05-28 22:10:50 +0100

    f4 added

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function retrive associated with key.
+ *
+ * @ht: hash table.
+ * @key: key of hash table
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned int index;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	if (node == NULL)
+		return (NULL);
+
+	while (node != NULL)
+	{
+		if (strcmp(key, node->key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
