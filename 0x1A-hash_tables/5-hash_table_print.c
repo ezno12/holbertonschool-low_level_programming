d880faddaa4712ea8fc12952c7e2f831c714f2e2
tennin12 <bassembenyhia12@gmail.com>
2021-05-28 22:52:32 +0100

    f5 added

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function print a hash table.
+ *
+ * @ht: hash table.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	char *sep;
+	unsigned int num_elements;
+	unsigned int i;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	i = 0;
+	num_elements = 0;
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+			num_elements++;
+		i++;
+	}
+
+	if (num_elements > 0)
+		printf("{");
+
+	i = 0;
+	sep = "";
+	while (i < ht->size)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			printf("%s", sep);
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+			sep = ", ";
+		}
+		i++;
+	}
+
+	if (num_elements > 0)
+		printf("}");
+	printf("\n");
+}
