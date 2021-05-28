969a36cd6029f010ee4478afa96a02b079fbc4a4
tennin12 <bassembenyhia12@gmail.com>
2021-05-28 23:06:07 +0100

    new files update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,26 +9,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned int index;
-
-	if (ht == NULL || key == NULL || strlen(key) == 0)
-		return (NULL);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	node = ht->array[index];
-
-	if (node == NULL)
-		return (NULL);
-
-	while (node != NULL)
-	{
-		if (strcmp(key, node->key) == 0)
-		{
-			return (node->value);
-		}
-		node = node->next;
-	}
-	return (NULL);
+hash_node_t *node;
+unsigned int index;
+if (ht == NULL || key == NULL || strlen(key) == 0)
+return (NULL);
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+if (node == NULL)
+return (NULL);
+while (node != NULL)
+{
+if (strcmp(key, node->key) == 0)
+{
+return (node->value);
+}
+node = node->next;
+}
+return (NULL);
 }
