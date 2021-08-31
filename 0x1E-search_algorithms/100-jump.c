baf485f07a92fca7019d0a0017ed6a5fe270a67f
tennin12 <bassembenyhia12@gmail.com>
2021-08-31 20:02:20 +0100

    f100 added

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - function that searches for a value in a sorted array of int.
+ *
+ *  @array: array of elements.
+ *  @size: size of the array.
+ *  @value: the element to search for.
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int jump_search(int *array, size_t size, int value)
+{
+size_t i = 0;
+size_t x;
+size_t m = sqrt(size); /* initializing block size= √(n) */
+
+if (array == NULL)
+{
+return (-1);
+}
+while (array[m] <= value && m < size)
+{
+/* the control will continue to jump the blocks */
+i = m; /* shift the block */
+m += sqrt(size);
+printf("Value checked array [%zu] = [%zu]\n", i, m);
+if (m > size - 1)
+{
+return (-1);
+}
+}
+printf("Value found between indexes [%zu] and [%zu]\n", i, m);
+for (x = i; x < m; x++)
+{
+/* linear search in current block */
+if (array[x] == value)
+{
+return (x); /* position of element being searched */
+}
+}
+return (-1);
+}
