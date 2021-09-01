bdd7fbd99f47302ec4fb726b6c63500ae21b9646
tennin12 <bassembenyhia12@gmail.com>
2021-09-01 13:31:22 +0100

    header file update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,9 +7,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_range(size_t start, size_t end, int *array);
 int exponential_search(int *array, size_t size, int value);
-int ranged_binary_search(int *array, size_t size, int value, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 
 #endif
